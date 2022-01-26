abc4389c033172c9e063e9168e411f7e95f7c6c5
Nicolas <naquiroz@uc.cl>
2022-01-26 15:57:39 -0300

    🎉  Begin lookup library

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "~/types"
+export { lookup as solidIconLookup } from "~/solidIcons"
+export { lookup as outlineIconLookup } from "~/outlineIcons"
+export { lookupIcon } from "./lookupIcon"
---
+++ src/lookupIcon.ts
@@ -0,0 +1,21 @@
+import { lookup as solidLookup } from "~/solidIcons"
+import { lookup as outlineLookup } from "~/outlineIcons"
+import { HeroIcon, IconFormat, PossibleIcons, PossibleIconsArray } from "./types"
+
+/**
+ * Lookup an icon by its name. Useful when you want to import dynamically an icon, from a database, for instance.
+ * @param iconName The name of the possible icon you wish to import dynamically. If the name does not exist, it will throw an error.
+ * @param format The format of the icon you wish to import.
+ * @returns A hero icon react component that you can render.
+ */
+export const lookupIcon = (iconName: PossibleIcons, format: IconFormat = "solid"): HeroIcon => {
+  if (!(iconName in PossibleIconsArray)) {
+    throw Error(
+      `The icon name '${iconName}' you provided does not exist in the library. Please check the name and try again.`
+    )
+  }
+  if (format === "solid") {
+    return solidLookup[iconName]
+  }
+  return outlineLookup[iconName]
+}
